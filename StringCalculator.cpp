1e3317c02b26e13d1b6da3893b4a6a27b1f7e6a3
armandopinedam <111319135+armandopinedam@users.noreply.github.com>
2024-11-26 10:46:51 -0600

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -5,5 +5,12 @@ int add(string input)
     if (input.empty()) {
         return 0;
     }
+
+    for (char ch : input) {
+        if (!isdigit(ch)) {
+            return 0;
+        }
+    }
+    
   throw "invalid argument";
 }
